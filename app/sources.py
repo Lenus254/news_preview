081cf35ebf5395ead3d1249b62e9c95491c2abd2
lenus254 <linus.cheruiyot@student.moringaschool.com>
2022-02-01 04:24:25 +0300

    created sources class and initialized it

---
+++ app/sources.py
@@ -0,0 +1,11 @@
+class Source:
+    '''
+    Source class to define source objects
+    '''
+
+    def __init__(self, id, title, name, description, url):
+        self.id = id
+        self.title = title
+        self.name = name
+        self.description = description
+        self.url = url
