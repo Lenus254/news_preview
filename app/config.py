fc5120e7991eeb24dc2e1f48bf295fb77a3b2155
lenus254 <linus.cheruiyot@student.moringaschool.com>
2022-02-01 05:27:23 +0300

    imported source class to request and defined sources funstion

---
+++ app/config.py
@@ -0,0 +1,40 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+
+    # CAT_API_URL='https://newsapi.org/v2/everything?q={}&sortBy=relevancy&apiKey={}'
+    API_KEY=os.environ.get('API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    NEWS_API_SOURCE_URL = 'https://newsapi.org/v2/sources?apiKey={}'.format(API_KEY)
+    CAT_API_URL='https://newsapi.org/v2/top-headlines?country=us&category={}&apiKey={}'
+
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
